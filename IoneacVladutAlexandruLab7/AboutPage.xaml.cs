6b7e221c6429c277e2731aa2989f38b74c91ff8e
vlad_ <vlad_@DESKTOP-9K38OAP>
2024-12-07 12:40:03 +0200

    Add project files.

---
+++ IoneacVladutAlexandruLab7/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace IoneacVladutAlexandruLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ IoneacVladutAlexandruLab7/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace IoneacVladutAlexandruLab7;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
